9cf4eeed3dd869e4ae5bf08b56ef8582eedfd19a
Starostin Nikita <Starostin.450503@gmail.com>
2018-10-27 14:21:22 +0300

    saimm lab2 part2

--- src/app/saimm/lab1/lab1.component.ts
+++ src/app/saimm/lab1/lab1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import * as mathjs from 'mathjs';
+
+type ChartType = 'bar';
 
 interface ILemmRandomDebugInfo {
   n: number,
@@ -9,46 +10,163 @@ interface ILemmRandomDebugInfo {
   x_curr: number;
 }
 
+interface ILemmInput {
+  a: number;
+  m: number;
+  r0: number;
+  amount: number;
+  bigAmount: number;
+}
+
+interface IRandomNumberProperties {
+  M: number;
+  D: number;
+  SKO: number;
+  period_length: number;
+  aperiodichnost_length: number;
+}
+
+interface IBarChart {
+  type: ChartType;
+  datasets: [
+    {
+      label: string;
+      data: number[];
+    }
+  ];
+  options: {
+    legend: {
+      display: boolean;
+    }
+    title: {
+      display: boolean;
+      text: string;
+    }
+  },
+  labels: string[];
+}
+
 @Component({
   selector: 'app-lab1',
   templateUrl: './lab1.component.html',
   styleUrls: ['./lab1.component.scss']
 })
 export class Lab1Component implements OnInit {
-
-  lemmInput = {
+  lemmInput: ILemmInput = {
     a: 3,
     m: 5,
     r0: 1,
     amount: 5,
+    bigAmount: 50000
   };
-
+  barChart: IBarChart = {
+    datasets: [
+      {
+        data: [],
+        label: 'Чисел в интервале'
+      }
+    ],
+    options: {
+      legend: {
+        display: true
+      },
+      title: {
+        display: true,
+        text: 'Гистограмма'
+      }
+    },
+    labels: [],
+    type: 'bar'
+  };
+  randomProperties: IRandomNumberProperties = {} as IRandomNumberProperties;
   lemmRandomDebugInfo: ILemmRandomDebugInfo[] = [];
+  lemmBigAmount = 50000;
 
   constructor() { }
 
   ngOnInit() {
-    this.countLemm();
+    this.onLemmInputChange();
   }
 
-  countLemm() {
+  onLemmInputChange() {
     const { a, m, r0, amount } = this.lemmInput;
-    const lemms: ILemmRandomDebugInfo[] = [];
-    const scope = {
-      a, m, r_prev: r0
+    if (!a || !m || !r0) {
+      return;
+    }
+    var t0 = performance.now();
+    const lemms = this.getLemms(a, m, r0, this.lemmBigAmount);
+    var t1 = performance.now();
+    console.log('Time for generating 50000 numbers: ' + (t1 - t0));
+    const nums = lemms.map(lem => lem.x_curr);
+    this.lemmRandomDebugInfo = this.getLemms(a, m, r0, amount);
+    var t3 = performance.now();
+    this.barChart.datasets[0].data = this.getDataForGistogramm(20, nums)
+    this.barChart.labels = [];
+    for (let i = 0; i < this.barChart.datasets[0].data.length; ++i) {
+      this.barChart.labels.push((i + 1).toString());
+    }
+    var t4 = performance.now();
+    console.log('Time for generating gistogram data: ' + (t4 - t3));
+    this.randomProperties = this.getRandomProperties(nums);
+  }
+
+  getDataForGistogramm(intervalsAmount: number, nums: number[]): number[] {
+    const step = 1 / intervalsAmount;
+    const res: number[] = [];
+    for (let i = 0; i < 1; i += step) {
+      res.push(nums.filter(num => num >= i && num < i + step).length);
+    }
+    return res;
+  }
+
+  getRandomProperties(nums: number[]): IRandomNumberProperties {
+    const M = this.getM(nums);
+    const D = this.getD(nums, M);
+    return {
+      aperiodichnost_length: this.getAperiodichnostLength(nums),
+      D,
+      M,
+      period_length: this.getPeriodLength(nums),
+      SKO: this.getSKO(D)
     };
-    for(let i = 0; i< amount; ++i) {
+  }
+
+  getM(nums: number[]): number {
+    const summ = nums.reduce((prev, curr) => prev + curr, 0);
+    return summ / nums.length;
+  }
+
+  getD(nums: number[], m: number): number {
+    const summ = nums.reduce((prev, curr) => prev + Math.pow((curr - m), 2), 0);
+    return summ / nums.length;
+  }
+
+  getSKO(d: number): number {
+    return Math.sqrt(d);
+  }
+
+  getAperiodichnostLength(nums: number[]): number {
+    return null;
+  }
+
+  getPeriodLength(nums: number[]): number {
+    return null;
+  }
+
+  getLemms(a: number, m: number, r0: number, amount: number): ILemmRandomDebugInfo[] {
+    const lemms: ILemmRandomDebugInfo[] = [];
+    let r_prev = r0;
+    for (let i = 0; i < amount; ++i) {
       const lemm: ILemmRandomDebugInfo = {
-         x_curr: mathjs.eval('a*r_prev%m/m', scope),
-         a_r_prev: mathjs.eval('a*r_prev', scope), 
-         n: i + 1,
-         r_curr: mathjs.eval('a*r_prev%m', scope), 
-         r_prev: scope.r_prev
+        x_curr: a * r_prev % m / m,
+        a_r_prev: a * r_prev,
+        n: i + 1,
+        r_curr: a * r_prev % m,
+        r_prev
       } as ILemmRandomDebugInfo;
       lemms.push(lemm);
-      scope.r_prev = lemm.r_curr;
+      r_prev = lemm.r_curr;
     }
-    this.lemmRandomDebugInfo = lemms;
-    console.log(lemms);
+    return lemms;
   }
 }
--- src/app/saimm/saimm.module.ts
+++ src/app/saimm/saimm.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SaimmRoutingModule } from './saimm-routing.module';
@@ -6,13 +6,16 @@ import { SaimmComponent } from './saimm.component';
 import { Lab1Component } from './lab1/lab1.component';
 import { Lab2Component } from './lab2/lab2.component';
 import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'angular-bootstrap-md';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    SaimmRoutingModule
+    SaimmRoutingModule,
+    ChartsModule
   ],
   declarations: [SaimmComponent, Lab1Component, Lab2Component],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class SaimmModule { }
